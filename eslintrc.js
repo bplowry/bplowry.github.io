0c2f27e5f454087cdd666978c251100c5701e172
Benjamin Lowry <bplowry@gmail.com>
2019-08-14 00:57:43 +0800

    Add eslint

---
+++ eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    "plugin:prettier/recommended",
+    "react-app",
+    "plugin:jsx-a11y/recommended",
+  ],
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+}
