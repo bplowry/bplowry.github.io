8b1b49c9c0afa1705c447ddd8d152e5c47e6d120
Benjamin Lowry <bplowry@gmail.com>
2019-08-14 01:42:16 +0800

    Add GA tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-142483220-2`,
       },
     },
     `gatsby-plugin-feed`,
