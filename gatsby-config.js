b27667dd943e18ce1ed7bae374d643e520bdf54c
Benjamin Lowry <bplowry@gmail.com>
2019-08-13 21:00:12 +0800

    Make it about me, not Kyle Mathews -- thanks!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Six of one`,
+    author: `Ben Lowry`,
+    description: `Thoughts on programming and perhaps other things.`,
+    siteUrl: `https://bplowry.github.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `benjaminlowry`,
     },
   },
   plugins: [
